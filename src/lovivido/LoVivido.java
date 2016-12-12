da25cb7745d0f57cf814e71f9b10026da2017b70
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-12-12 08:59:39 +0100

    Crear clase calcular horas y format

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -7,47 +7,40 @@ package lovivido;
 import javax.swing.JOptionPane;
 
 public class LoVivido {
-    
-public static void CalcM(int a,String n){
-    int m = (a*12);
-    JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
-}
 
-public static int CalcD(int a,String n){
-    int d = (a*365);
-    JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-    return d;
-}
+    public static void CalcM(int a, String n) {
+        int m = (a * 12);
+        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+    }
 
+    public static int CalcD(int a, String n) {
+        int d = (a * 365);
+        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        return d;
+    }
 
-public static void main(String[] args) {
-String n;
-String e;
+    public static void CalcH(int d, String n) {
+        int h = (d * 24);
+        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+    }
 
+    public static void main(String[] args) {
+        String n;
+        String e;
 
-int h;
-int a;
+        int a;
 
-n = JOptionPane.showInputDialog ("Escriba su nombre: ");
-e = JOptionPane.showInputDialog ("Escriba su edad: ");
- 
-a = Integer.parseInt (e);
- 
+        n = JOptionPane.showInputDialog("Escriba su nombre: ");
+        e = JOptionPane.showInputDialog("Escriba su edad: ");
 
+        a = Integer.parseInt(e);
 
-h = (d*24);
- 
- 
-CalcM(a,n);
-int aux=CalcD(a,n);
+        CalcM(a, n);
+        int aux = CalcD(a, n);
+        CalcH(aux, n);
 
+        System.exit(0);
 
-
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-   System.exit (0);
-
-}
-   
+    }
 
 }
